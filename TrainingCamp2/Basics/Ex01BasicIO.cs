5a8762cc4907c6584704502e8a7eed4fceaa7a94
Phani Raj <phaniraj.lms@gmail.com>
2026-06-03 12:47:00 +0530

    firstExample

---
+++ TrainingCamp2/Basics/Ex01BasicIO.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSharpBasics
+{
+    class Ex01BasicIO
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
