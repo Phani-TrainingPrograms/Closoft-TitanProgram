820da7d3a07299df0696823648caf5777e4af144
Phani Raj <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2026-06-11 15:44:10 +0530

    Runtime polymorphism

---
+++ TrainingCamp2/Titan-Induction/SampleConApp/Ex15InheritanceDemo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BaseClassLib;
+
+namespace SampleConApp
+{
+    class DerivedClass : BaseClass
+    {
+        public void DerivedTestFunc() => Console.WriteLine("Derived class Test Func");
+    }
+    internal class Ex15InheritanceDemo
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass cls = new DerivedClass();
+            cls.TestFunc();
+            cls.DerivedTestFunc();
+        }
+    }
+}
---
+++ TrainingCamp2/Titan-Induction/SampleConApp/Ex16MethodOverridingExample.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+//Method overriding is modifying a base class function in the dirived class. 
+//Method overriding leads to runtime polymorphism. Same function with the same object will behave in different manner.
+//The Base class function should be marked with virtual keyword
+//Derived class can rewrite that function using override keyword. 
+
+namespace SampleConApp
+{
+    class BaseClass
+    {
+        public virtual void TestFunc() => Console.WriteLine("Conducting sample base Test");
+    }
+
+    class SubClass : BaseClass
+    {
+        public override void TestFunc()
+        {
+            base.TestFunc();//Call the base functionality
+            Console.WriteLine("Conducting More tests added from Base");
+        }
+    }
+
+    class Activator
+    {
+        public static BaseClass GetObject(string type)
+        {
+            if(type == "Base")
+                return new BaseClass();
+            else if(type == "Sub")
+                return new SubClass();
+            else
+                throw new Exception("Invalid type");
+        }
+    }
+    internal class Ex16MethodOverridingExample
+    {
+        static void Main(string[] args)
+        {
+            //BaseClass cls = new BaseClass();
+            //cls.TestFunc();//Base version will be called. 
+
+            //cls = new SubClass();
+            //cls.TestFunc();//Derived version will be called
+
+
+            ///////////////////2nd version/////////////////////
+            Console.WriteLine("Enter the Type of object U want to create");
+            var type = Console.ReadLine();
+            BaseClass obj = Activator.GetObject(type);
+            obj.TestFunc();
+        }
+    }
+}
